583600804690d1b01801152f9c7272b20196e154
khanvivy <vivykhan@gmail.com>
2022-04-15 05:59:12 -0700

    print format specifiers

--- _printf.c
+++ _printf.c
@@ -35,6 +35,56 @@ static int (*check_for_specifiers(const char *formart))(va_list)
 	return (p[i].f);
 }
 
+/**
+ * _printf - function for printing format 
+ * @format: list arguments for printing
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int (*f)(va_list);
+	unsigned int i = 0, counter = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			counter++;
+			i++;
+			continue;
+		}
+		else
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				counter++;
+				i += 2;
+				continue;
+			}
+			else
+			{
+				f = check_for_specifiers(&format[i + 1]);
+				if (f == NULL)
+					return (-1);
+				i += 2;
+				counter += f(ap);
+				continue;
+			}
+		}
+		i++;
+	}
+	va_end(ap);
+	return (counter);
+}
+
+
 
 
 
